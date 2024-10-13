3870f127c5eea3d5f5fe30e4eb1a7447e87739c3
0sakana3 <shakex3.tp3@gmail.com>
2024-10-13 22:55:20 +0900

    新規登録入力エラー文の重複と順序の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :date_of_birth, presence: true
-
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'Must contain both half-width alphanumeric characters' }
-
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
   VALID_NAMEKANA_REGEX = /\A[ァ-ヶー－]+\z/
+
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'Must contain both half-width alphanumeric characters' }
+  validates :nickname, presence: true
+
+  validates :family_name, presence: true
   validates :family_name, format: { with: VALID_NAME_REGEX, message: 'Full-width (Kanji, Hiragana, Katakana) required' }
+
+  validates :first_name, presence: true
   validates :first_name, format: { with: VALID_NAME_REGEX, message: 'Full-width (Kanji, Hiragana, Katakana) required' }
+
+  validates :family_name_kana, presence: true
   validates :family_name_kana, format: { with: VALID_NAMEKANA_REGEX, message: 'Full-width (Katakana) required' }
+
+  validates :first_name_kana, presence: true
   validates :first_name_kana, format: { with: VALID_NAMEKANA_REGEX, message: 'Full-width (Katakana) required' }
+
+  validates :date_of_birth, presence: true
 end
