7c81f2f991911aa1b264b49b0d419a326067cfd5
0sakana3 <shakex3.tp3@gmail.com>
2024-10-14 16:35:33 +0900

    正常系テスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,60 +5,67 @@ RSpec.describe User, type: :model do
   end
 
   describe '新規登録/ユーザー情報' do
-    it 'ニックネームが必須であること' do
-      @user.nickname = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Nickname can't be blank")
-    end
-    it 'メールアドレスが必要であること' do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
-    end
-    it 'メールアドレスが一意性であること' do
-      @user.save
-      another_user = FactoryBot.build(:user)
-      another_user.email = @user.email
-      another_user.valid?
-      expect(another_user.errors.full_messages).to include('Email has already been taken')
-    end
-    it 'メールアドレスは、＠を含む必要があること' do
-      @user.email = 'testmail'
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Email is invalid')
-    end
-    it 'パスワードが必須であること' do
-      @user.password = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
-    end
-    it 'パスワードは、６文字以上での入力が必須であること' do
-      @user.password = '1234b'
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
-    end
-    context 'パスワードは、半角英数混合での入力が必須であること' do
-      it 'パスワードは、半角英字のみでないこと' do
-        @user.password = 'aaaaaa'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password Must contain both half-width alphanumeric characters')
-      end
-      it 'パスワードは、半角数字のみでないこと' do
-        @user.password = '000000'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password Must contain both half-width alphanumeric characters')
-      end
-      it 'パスワードは、半角での入力が必須であること' do
-        @user.password = 'ああああああ'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password Must contain both half-width alphanumeric characters')
+    context 'ユーザー登録ができる時' do
+      it '必要な情報を適切に入力して「会員登録」ボタンを押すと、ユーザーの新規登録ができる' do
+        expect(@user).to be_valid
       end
     end
-    it 'パスワードとパスワード（確認）は、値の一致が必須であること' do
-      @user.password = '123456a'
-      @user.password_confirmation = '00000b'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    context 'ユーザー登録ができない時' do
+      it 'ニックネームが必須であること' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'メールアドレスが必要であること' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'メールアドレスが一意性であること' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'メールアドレスは、＠を含む必要があること' do
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it 'パスワードが必須であること' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'パスワードは、６文字以上での入力が必須であること' do
+        @user.password = '1234b'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+      context 'パスワードは、半角英数混合での入力が必須であること' do
+        it 'パスワードは、半角英字のみでないこと' do
+          @user.password = 'aaaaaa'
+          @user.valid?
+          expect(@user.errors.full_messages).to include('Password Must contain both half-width alphanumeric characters')
+        end
+        it 'パスワードは、半角数字のみでないこと' do
+          @user.password = '000000'
+          @user.valid?
+          expect(@user.errors.full_messages).to include('Password Must contain both half-width alphanumeric characters')
+        end
+        it 'パスワードは、半角での入力が必須であること' do
+          @user.password = 'ああああああ'
+          @user.valid?
+          expect(@user.errors.full_messages).to include('Password Must contain both half-width alphanumeric characters')
+        end
+      end
+      it 'パスワードとパスワード（確認）は、値の一致が必須であること' do
+        @user.password = '123456a'
+        @user.password_confirmation = '00000b'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
     end
   end
 
