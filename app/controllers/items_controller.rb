61ac9e2e24b0696c0585ef606343ea9c46e7c5f6
0sakana3 <shakex3.tp3@gmail.com>
2024-10-20 16:47:52 +0900

    商品出品機能　商品の出品

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,15 +4,22 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :image, :explanation, :category_id, :situation_id , :shipping_fee_id, :prefecture_id, :time_required_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,19 +2,19 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
-  belongs_to :category
-  # belongs_to :situation
-  # belongs_to :shipping_fee
-  # belongs_to :prefecture
-  # belongs_to :time_required
 
-  validates :item_name, :string, presence: true
-  validates :explanation, :text, presence: true
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :time_required
+
+  validates :item_name, presence: true
+  validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  # validates :situation_id, numericality: { other_than: 1, message: "can't be blank" }
-  # validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
-  # validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  # validates :time_required_id, numericality: { other_than: 1, message: "can't be blank" }
-  # validates :price, :integer, presence: true
-  # validates :user, :references, presence: true
+  validates :situation_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :time_required_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true
 end
