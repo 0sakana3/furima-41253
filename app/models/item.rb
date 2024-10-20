db4e5261af05c665d8f0b0ae60f648b7a21b4499
0sakana3 <shakex3.tp3@gmail.com>
2024-10-20 19:28:47 +0900

    商品出品機能　エラー文表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,6 +19,6 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :time_required_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, format: { with: VALID_PRICE_REGEX, message: 'Half-width (digits) required' }
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Please register between 300 yen and 9,999,999 yen' }
+  validates :price, presence: true, format: { with: VALID_PRICE_REGEX, message: 'Half-width (digits) required' },
+                                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Please register between 300 yen and 9,999,999 yen' }
 end
