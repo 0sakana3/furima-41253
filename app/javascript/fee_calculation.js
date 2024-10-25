901791ac8491f50e5decbe792c42b3a4ca9051fb
0sakana3 <shakex3.tp3@gmail.com>
2024-10-25 22:55:12 +0900

    Math.roundをMath.flooへ変更

--- app/javascript/fee_calculation.js
+++ app/javascript/fee_calculation.js
@@ -1,15 +1,15 @@
 const price = () => {
 
-window.addEventListener('keyup', () => {
+window.addEventListener('load', () => {
   const priceInput = document.getElementById( "item-price" );
 
   priceInput.addEventListener( 'input',()  => {
     const feeDisplay = document.getElementById( "add-tax-price" );
-    feeDisplay.innerHTML = Math.round ( priceInput.value * 0.1 );
+    feeDisplay.innerHTML = Math.floor ( priceInput.value * 0.1 );
     
     const profitDisplay = document.getElementById("profit");
-    profitDisplay.innerHTML = Math.round( priceInput.value - Math.floor( priceInput.value * 0.1 ))
-});
+    profitDisplay.innerHTML = Math.floor( priceInput.value - Math.floor( priceInput.value * 0.1 ))
+})
 });
 
 };
