384e391e0f3295cfa079060b49ef53eb38980df7
0sakana3 <shakex3.tp3@gmail.com>
2024-11-09 22:46:01 +0900

    Formオブジェクトでの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,28 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @orders_address = OrdersAddress.new
+  end
+
+  def new
+    @orders_address = OrdersAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @orders_address = OrdersAddress.new(orders_params)
+    if @orders_address.valid?
+      @orders_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def orders_params
+    params.require(:orders_address).permit(:post_code, :prefecture_id, :order, :city, :address, :building_name,
+                                           :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
---
+++ app/models/orders_address.rb
@@ -0,0 +1,21 @@
+class OrdersAddress
+  include ActiveModel::Model
+  attr_accessor :item_id, :user_id, :oreders_id, :post_code, :prefecture_id, :city, :address, :building_name,
+                :phone_number
+
+  VALID_POST_CODE_REGEX = /\A\d{3}-\d{4}\z/
+  VALID_PHONE_NUMBER = /\A\d{10,11}\z/
+  with_options presence: true do
+    validates :post_code, format: { with: VALID_POST_CODE_REGEX, message: 'is invalid. Include hyphen(-)' }
+    validates :city
+    validates :address
+    validates :phone_number,
+              format: { with: VALID_PHONE_NUMBER, message: 'is invalid.Please enter 10-11 digit half-width numbers' }
+  end
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  def save
+    order = Order.create(item_id:, user_id:)
+    Address.create(post_code:, prefecture_id:, city:, address:, building_name:, phone_number:, order_id: order.id)
+  end
+end
