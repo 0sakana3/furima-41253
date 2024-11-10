ae8d6ce0f41dfa5b8487d33078ede2dcc1088a70
0sakana3 <shakex3.tp3@gmail.com>
2024-11-10 14:34:11 +0900

    商品購入ページに入れるユーザーの制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :new, :create]
+  before_action :orderer_confirmation, only: [:index, :new, :create]
   def index
     @item = Item.find(params[:item_id])
     @orders_address = OrdersAddress.new
@@ -24,5 +27,13 @@ class OrdersController < ApplicationController
   def orders_params
     params.require(:orders_address).permit(:post_code, :prefecture_id, :order, :city, :address, :building_name,
                                            :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def orderer_confirmation
+    redirect_to root_path if current_user.id == @item.user_id
   end
 end
