2295deebe41c9419c0569849a2e9413b9c1b0f07
0sakana3 <shakex3.tp3@gmail.com>
2024-12-28 18:52:33 +0900

    テストコード

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'いいね機能' do
+    context 'いいねができる場合' do
+      it 'user_idとitem_idがあれば保存できる' do
+        expect(@like).to be_valid
+      end
+      it 'item_idが同じでもuser_idが違えばいいねできる' do
+      end
+      it 'user_idが同じでもitem_idが違えばいいねできる' do
+      end
+    end
+    context 'いいねができない場合' do
+      it 'user_idが空ではいいねできない' do
+      end
+      it 'item_idが空ではいいねできない' do
+      end
+    end
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,20 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Like, type: :model do
-  describe 'いいね機能' do
-    context 'いいねができる場合' do
-      it 'user_idとitem_idがあれば保存できる' do
-      end
-      it 'item_idが同じでもuser_idが違えばいいねできる' do
-      end
-      it 'user_idが同じでもitem_idが違えばいいねできる' do
-      end
-    end
-    context 'いいねができない場合' do
-      it 'user_idが空ではいいねできない' do
-      end
-      it 'item_idが空ではいいねできない' do
-      end
-    end
-  end
+RSpec.describe Order, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
